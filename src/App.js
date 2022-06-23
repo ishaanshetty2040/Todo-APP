fa6a70a9ae029620c5f6db47bbebace44c4c2ca9
Ishaan <ishaanshetty12@gmail.com>
2022-06-23 21:45:34 +0530

    Files modified

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Heading } from "@chakra-ui/react";
 import TodoList from "./Components/TodoList";
 import AddTodo from "./Components/AddTodo";
 import { VStack } from "@chakra-ui/react";
-import { IconButton } from "@chakra-ui/react";
+import { IconButton, useColorMode } from "@chakra-ui/react";
 import { FaSun, FaMoon } from "react-icons/fa";
 import { useState, useEffect } from "react";
 
@@ -43,13 +43,16 @@ function App() {
     setTodos([...todos, todo]);
   }
 
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
     <VStack p={4}>
       <IconButton
-        icon={<FaSun />}
+        icon={colorMode === "light" ? <FaSun /> : <FaMoon />}
         isRound="true"
         size="lg"
         alignSelf="flex-end"
+        onClick={toggleColorMode}
       />
       <Heading
         mb="8"
@@ -58,7 +61,7 @@ function App() {
         bgGradient="linear(to-r, pink.500,blue.500)"
         bgClip="text"
       >
-        Todo Applications
+        Todo List
       </Heading>
       ;
       <TodoList todos={todos} deleteTodo={deleteTodo} /> {/* using prop */}
